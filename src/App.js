05d448a9c6b4359b76a99b58b48c12027c990c02
BaeInHoo <qodlsgh0210@naver.com>
2022-10-12 00:38:44 +0900

    create number icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Backspace } from 'tabler-icons-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='cal-app'>
+      <div className='output'>
+        <button>C</button>
+        <button></button>
+        <button>=</button>
+        <button>
+          <Backspace size={48} strokeWidth={2} color={'black'} />
+        </button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>x</button>
+        <button>0</button>
+        <button>-</button>
+        <button>+</button>
+      </div>
     </div>
   );
 }
