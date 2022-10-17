5c2fa1c174104a4fc2e333b5eedb7942dd6ef55c
BaeInHoo <qodlsgh0210@naver.com>
2022-10-17 17:22:25 +0900

    v1.0 wrap up

--- src/App.js
+++ src/App.js
@@ -2,35 +2,37 @@ import './App.scss';
 import { Backspace } from 'tabler-icons-react';
 import { useReducer } from 'react';
 import { reducer } from './reducers/reducer';
+import DigitButton from './components/DigitButton';
+import OperationButton from './components/OperationButton';
 
 function App() {
   const [{ currentOperand }, dispatch] = useReducer(reducer, {currentOperand: 0});
 
   return (
     <div className='cal-app'>
-      <div className='output'>30233</div>
+      <div className='output'>{ currentOperand }</div>
       <button style={{ opacity: 0 }}></button>
-      <button className='operation'>C</button>
-      <button className='operation'>=</button>
-      <button className='operation'>
+      <button className='operation' onClick={() => dispatch({ type: 'clear' })}>C</button>
+      <button className='operation' onClick={() => dispatch({ type: 'result' })}>=</button>
+      <button className='operation' onClick={() => dispatch({ type: 'remove' })}>
         <Backspace size={42} strokeWidth={2} color={'white'} />
       </button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button className='operation'>%</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button className='operation'>/</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button className='operation'>x</button>
-      <button>0</button>
-      <button>.</button>
-      <button className='operation'>-</button>
-      <button className='operation'>+</button>
+      <DigitButton digit="1" dispatch={dispatch}/>
+      <DigitButton digit="2" dispatch={dispatch}/>
+      <DigitButton digit="3" dispatch={dispatch}/>
+      <OperationButton operation="%" dispatch={dispatch}/>
+      <DigitButton digit="4" dispatch={dispatch}/>
+      <DigitButton digit="5" dispatch={dispatch}/>
+      <DigitButton digit="6" dispatch={dispatch}/>
+      <OperationButton operation="/" dispatch={dispatch}/>
+      <DigitButton digit="7" dispatch={dispatch}/>
+      <DigitButton digit="8" dispatch={dispatch}/>
+      <DigitButton digit="9" dispatch={dispatch}/>
+      <OperationButton operation="x" dispatch={dispatch}/>
+      <DigitButton digit="0" dispatch={dispatch}/>
+      <DigitButton digit="." dispatch={dispatch}/>
+      <OperationButton operation="-" dispatch={dispatch}/>
+      <OperationButton operation="+" dispatch={dispatch}/>
     </div>
   );
 }
